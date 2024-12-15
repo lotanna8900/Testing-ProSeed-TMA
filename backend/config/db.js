6a3b017ee88747332741773c5e510dd87c7b9f36
lotanna8900 <lotanna8900@gmail.com>
2024-12-15 10:11:47 +0100

    Update db.js to log MongoDB URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    console.error("MongoDB URI not provided!");
+    process.exit(1);
+  } else {
+    console.log("MongoDB URI is provided:", uri);
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
@@ -11,4 +25,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
